26256c906a17710a41f453324d905d6db1f4b582
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-24 22:44:01 +0800

    create navigation for main app component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,5 +1,13 @@
+import AppNav from "../components/AppNav";
+
 function AppLayout() {
-    return <div>APP</div>;
+    return (
+        <div>
+            <AppNav />
+
+            <p>App</p>
+        </div>
+    );
 }
 
 export default AppLayout;
