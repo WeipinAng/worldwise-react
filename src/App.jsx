ef08b7ba36224dfa0d304d78655638efc3e167d0
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-27 14:07:52 +0800

    build list of all countries that the user has already visited (by deriving countries from cities and filtering out duplicate countries)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -6,7 +8,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 polyfillCountryFlagEmojis();
@@ -53,7 +55,15 @@ function App() {
                             <CityList cities={cities} isLoading={isLoading} />
                         }
                     />
-                    <Route path="countries" element={<p>Countries</p>} />
+                    <Route
+                        path="countries"
+                        element={
+                            <CountryList
+                                cities={cities}
+                                isLoading={isLoading}
+                            />
+                        }
+                    />
                     <Route path="form" element={<p>Form</p>} />
                 </Route>
                 <Route path="*" element={<PageNotFound />} />
