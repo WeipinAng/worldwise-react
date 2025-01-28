8041364a14e6abdcc5f298629723a885cd336999
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-28 17:59:16 +0800

    fix bug of only a flash moving back from form component after clicking back button due to page reloading

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -56,7 +56,13 @@ function Form() {
 
             <div className={styles.buttons}>
                 <Button type="primary">Add</Button>
-                <Button type="back" onClick={() => navigate(-1)}>
+                <Button
+                    type="back"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        navigate(-1);
+                    }}
+                >
                     &larr; Back
                 </Button>
             </div>
