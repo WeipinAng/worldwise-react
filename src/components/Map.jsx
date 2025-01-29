5a3fd20ff12a26e2b6097673e1b123a1b979be4d
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-29 18:04:00 +0800

    open City component and also set position of the map to the currently selected city by creating a custom component (useEffect for synchronization after lat and lng disappear from url)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
@@ -11,15 +11,22 @@ function Map() {
 
     const [mapPosition, setMapPosition] = useState([40, 0]);
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
+    const [searchParams] = useSearchParams();
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(
+        function () {
+            if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+        },
+        [mapLat, mapLng]
+    );
 
     return (
         <div className={styles.mapContainer}>
             <MapContainer
                 center={mapPosition}
-                zoom={13}
+                zoom={6}
                 scrollWheelZoom={true}
                 className={styles.map}
             >
@@ -38,9 +45,17 @@ function Map() {
                         </Popup>
                     </Marker>
                 ))}
+
+                <ChangeCenter position={mapPosition} />
             </MapContainer>
         </div>
     );
+}
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
 }
 
 export default Map;
