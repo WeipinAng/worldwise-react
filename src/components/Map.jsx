8858ed425577030b43bb762286918e1f5fd85d1a
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-29 20:15:05 +0800

    create a custom hook called useUrlPosition by extracting some stateful logic

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,21 +11,19 @@ import styles from "./Map.module.css";
 import Button from "./Button";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
-    const [searchParams] = useSearchParams();
     const {
         isLoading: isLoadingPosition,
         position: geolocationPosition,
         getPosition,
     } = useGeolocation();
-
-    const mapLat = searchParams.get("lat");
-    const mapLng = searchParams.get("lng");
+    const [mapLat, mapLng] = useUrlPosition();
 
     useEffect(
         function () {
