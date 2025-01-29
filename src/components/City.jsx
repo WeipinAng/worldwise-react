6a871bda3ae8b51ad500ba62f4c9aea5063fa732
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-29 12:54:07 +0800

    add a button to go back from individual city component without having to click on 'cities' tab

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
 import styles from "./City.module.css";
 import Spinner from "./Spinner";
+import BackButton from "./BackButton";
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -59,7 +60,9 @@ function City() {
                 </a>
             </div>
 
-            <div></div>
+            <div>
+                <BackButton />
+            </div>
         </div>
     );
 }
