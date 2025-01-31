8358c471e16405186f89b05d21db4c86be4e2d98
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-02-01 00:10:48 +0800

    connect useAuth custom hook with the login form button and broadcast to entire application tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
@@ -18,27 +19,29 @@ polyfillCountryFlagEmojis();
 
 function App() {
     return (
-        <CitiesProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Homepage />} />
-                    <Route path="product" element={<Product />} />
-                    <Route path="pricing" element={<Pricing />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="app" element={<AppLayout />}>
-                        <Route
-                            index
-                            element={<Navigate replace to="cities" />}
-                        />
-                        <Route path="cities" element={<CityList />} />
-                        <Route path="cities/:id" element={<City />} />
-                        <Route path="countries" element={<CountryList />} />
-                        <Route path="form" element={<Form />} />
-                    </Route>
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </BrowserRouter>
-        </CitiesProvider>
+        <AuthProvider>
+            <CitiesProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Homepage />} />
+                        <Route path="product" element={<Product />} />
+                        <Route path="pricing" element={<Pricing />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="app" element={<AppLayout />}>
+                            <Route
+                                index
+                                element={<Navigate replace to="cities" />}
+                            />
+                            <Route path="cities" element={<CityList />} />
+                            <Route path="cities/:id" element={<City />} />
+                            <Route path="countries" element={<CountryList />} />
+                            <Route path="form" element={<Form />} />
+                        </Route>
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </CitiesProvider>
+        </AuthProvider>
     );
 }
 
